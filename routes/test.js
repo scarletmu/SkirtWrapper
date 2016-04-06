99c6f9446bedfd737adfa1a824aa72262815faa6
scarletmu@126.com <scarletmu@126.com>
2016-04-06 17:52:23 +0800

    validate it working

--- routes/test.js
+++ routes/test.js
@@ -7,10 +7,22 @@ var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request('http://ailand-store.jp/ap/s/s?rows=40&fq=bd:AL040&fq=scls:n_usual&sort=fsdt+desc&fq=-stnum:0&fq=-icon:restock',function(err,response,body){
-    let $ = cheerio.load(body);
-    let list = $('#itemContainer').children();
-    console.log(list);
+  let params = new Array();
+  params.push('wt=json');
+  params.push('fq=bd%3AAL040');
+  params.push('fq=scls%3An_usual');
+  params.push('fq=-stnum%3A0');
+  params.push('fq=-icon%3Arestock');
+  params.push('rows=40');
+  params.push('sort=fsdt%20desc');
+  params.push('facet=true');
+  params.push('facet.field=cc1');
+  let url = 'http://ailand-store.jp/ise/select?'+params.join('&');
+  console.log(url);
+  request(url,function(err,response,body){
+    let data = JSON.parse(body);
+    let itemList = data.response.docs;
+    console.log(itemList);
     res.status(200).end();
   })
 });
