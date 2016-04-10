ba23d82beaed13c02705bd2156e44564f4d5fde7
scarletmu <scarletmu@126.com>
2016-04-10 22:28:28 +0800

    change

--- routes/ankrouge.js
+++ routes/ankrouge.js
@@ -10,7 +10,24 @@ var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  let params = [];
+  params.push('wt=json');
+  params.push('fq=bd%3AAL040');
+  params.push('fq=scls%3An_usual');
+  params.push('fq=-stnum%3A0');
+  params.push('fq=-icon%3Arestock');
+  params.push('rows=40');
+  params.push('sort=fsdt%20desc');
+  params.push('facet=true');
+  params.push('facet.field=cc1');
+  let url = 'http://ailand-store.jp/ise/select?'+params.join('&');
+  console.log(url);
+  request(url,function(err,response,body){
+    let data = JSON.parse(body);
+    let itemList = data.response.docs;
+    console.log(itemList);
+    res.status(200).end();
+  })
 });
 module.exports = router;
 
