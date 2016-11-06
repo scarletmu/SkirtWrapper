666f8b3dd28a5856ecbc35410256636815112f0d
ScarletMu <scarletmu@126.com>
2016-11-06 16:06:06 +0800

    change calendar

--- model/calendar.js
+++ model/calendar.js
@@ -3,9 +3,4 @@ const Calendar = require('./db/mongo').CalendarModel;
 
 exports.saveNewArrivalDate = arg => Calendar.insert(arg);
 
-exports.getNewArrivalDate = page => {
-  let head = (page -  1) * 10;
-  return Calendar.find({}, null, {skip: head, limit: 10, sort: {createdAt: -1}});
-}
-
-exports.getNewArrivalLatest = () => Calendar.find({}, null, {sort: {createdAt: -1}})
+exports.getNewArrivalLatest = () => Calendar.find({}, null, {sort: {createdAt: -1}, limit: 5});
